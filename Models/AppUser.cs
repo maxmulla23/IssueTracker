b06c4b5a23d7bd136eb6a5921ab5e18fe55a81f2
maxmulla23 <maxmulla23@gmail.com>
2024-02-07 10:38:26 +0300

    identity user

---
+++ Models/AppUser.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace IssueTracker.Models
+{
+    public class AppUser : IdentityUser
+    {
+        
+    }
+}
