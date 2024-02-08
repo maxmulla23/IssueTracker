cf2da9b1aadddea4088438a6810d4fb97ed30531
maxmulla23 <maxmulla23@gmail.com>
2024-02-08 17:05:23 +0300

    commit

--- obj/Debug/net8.0/IssueTracker.AssemblyInfo.cs
+++ obj/Debug/net8.0/IssueTracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IssueTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03113c3a75c1aae9210888554d421e15416d7aec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b06c4b5a23d7bd136eb6a5921ab5e18fe55a81f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("IssueTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IssueTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
