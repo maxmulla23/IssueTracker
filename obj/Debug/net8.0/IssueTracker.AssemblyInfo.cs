5e8de763fde364d58ef76df21b209d0db7ed5ed2
maxmulla23 <maxmulla23@gmail.com>
2024-02-12 14:21:42 +0300

    migration

--- obj/Debug/net8.0/IssueTracker.AssemblyInfo.cs
+++ obj/Debug/net8.0/IssueTracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IssueTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+853cd15f29bbfb9ebb2da296a56cc51e71295869")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+936d12bb7399db5558df52127abaf5b0b9966b34")]
 [assembly: System.Reflection.AssemblyProductAttribute("IssueTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IssueTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
