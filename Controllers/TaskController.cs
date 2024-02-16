84b24a85fa4b0dc10390db16e92dd027da6e548d
maxmulla23 <maxmulla23@gmail.com>
2024-02-16 10:57:35 +0300

    delete todo

--- Controllers/TaskController.cs
+++ Controllers/TaskController.cs
@@ -82,6 +82,22 @@ namespace IssueTracker.Controllers
             return NoContent();
         }
 
+        //Delete task
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTodo(int id)
+        {
+            var todo = await _context.Todos.FindAsync(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            _context.Todos.Remove(todo);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
         private bool TodoExists(int id)
         {
             return _context.Todos.Any(e => e.Id == id);
