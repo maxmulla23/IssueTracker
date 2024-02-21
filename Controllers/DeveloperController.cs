dfc466a4e6be2e7df87192d3ed7794b419c28b34
maxmulla23 <maxmulla23@gmail.com>
2024-02-21 11:10:55 +0300

    get all developers and get by id

---
+++ Controllers/DeveloperController.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Linq;
+using IssueTracker.Data;
+using IssueTracker.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace IssueTracker.Controllers
+{
+    public class DeveloperController : ControllerBase
+    {
+        public readonly IssueTrackerContext _context;
+
+        public DeveloperController(IssueTrackerContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet] // get all developers
+        public async Task<ActionResult<IEnumerable<Developer>>> GetDeveloper()
+        {
+            return await _context.Developers.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+
+        public async Task<ActionResult<Developer>> GetDeveloper(int id)
+        {
+            var developer = await _context.Developers.FindAsync(id);
+
+            if(developer == null)
+            {
+                return NotFound();
+            }
+
+            return developer;
+        }
+    }
+}
--- obj/Debug/net8.0/IssueTracker.AssemblyInfo.cs
+++ obj/Debug/net8.0/IssueTracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IssueTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fc8d549685c73e8c50ef0b243b5fa52361bc114")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d4098e1096963070da7a23ef7ab595dc44ff9e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("IssueTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IssueTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
