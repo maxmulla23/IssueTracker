e041826534caa0f7cc3e31dd2dbe65f496cf6158
maxmulla23 <maxmulla23@gmail.com>
2024-03-03 16:04:41 +0300

    login and register model

---
+++ Models/LoginModel.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using IssueTracker.Data;
+
+namespace IssueTracker.Models
+{
+    public class LoginModel
+    {
+        
+        [Required(ErrorMessage ="Email is required")]
+        public string Email { get; set; }
+        [Required(ErrorMessage ="Password is required")]
+        public string Password { get; set; }
+      
+
+    }
+}
--- Models/RegisterModel.cs
+++ Models/RegisterModel.cs
@@ -12,6 +12,8 @@ namespace IssueTracker.Models
         [Required]
         public string Email { get; set; }
         [Required]
+        public string Password { get; set; }
+        [Required]
         public string PhoneNumber { get; set; }
         [Required]
         public UserType UserType { get; set; }
